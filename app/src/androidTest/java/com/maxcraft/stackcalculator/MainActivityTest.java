4a98be19f560c241b2f00dd7540e2e953e307fbd
marykravets <mary.kravets@gmail.com>
2018-09-12 22:09:56 +0300

    tests added (#2)
    * code review
    
    * moved GridLayout properties to style
    
    * requested changes added
    
    * requested changes added
    
    * requested changes added
    
    * requested changes added
    
    * requested changes added
    
    * requested changes added
    
    * tests added
    
    * update
    
    * strings corrected
    
    * format

---
+++ app/src/androidTest/java/com/maxcraft/stackcalculator/MainActivityTest.java
@@ -0,0 +1,315 @@
+package com.maxcraft.stackcalculator;
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.assertion.ViewAssertions.*;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void CleanBtnClickTest() {
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.btn_clear), withText(R.string.clear),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                0),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+        onView(withId(R.id.indicator_txt)).check(matches(withText("0")));
+    }
+
+    @Test
+    public void pushBtnTestClick() {
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.btn_push), withText(R.string.push),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                17),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+        onView(withId(R.id.indicator_txt)).check(matches(withText("0")));
+    }
+
+    @Test
+    public void swapBtnTestClick() {
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.btn_swap), withText(R.string.swap),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                18),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+        onView(withId(R.id.indicator_txt)).check(matches(withText("0")));
+    }
+
+    @Test
+    public void divTest() {
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.btn_9), withText(R.string.nine),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                9),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.btn_neg), withText(R.string.neg),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                16),
+                        isDisplayed()));
+        appCompatButton2.perform(click());
+
+        ViewInteraction appCompatButton3 = onView(
+                allOf(withId(R.id.btn_push), withText(R.string.push),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                17),
+                        isDisplayed()));
+        appCompatButton3.perform(click());
+
+        ViewInteraction appCompatButton4 = onView(
+                allOf(withId(R.id.btn_6), withText(R.string.six),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                6),
+                        isDisplayed()));
+        appCompatButton4.perform(click());
+
+        ViewInteraction appCompatButton5 = onView(
+                allOf(withId(R.id.btn_div), withText(R.string.div),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                15),
+                        isDisplayed()));
+        appCompatButton5.perform(click());
+
+        Locale fmtLocale = Locale.getDefault(Locale.Category.FORMAT);
+        NumberFormat formatter = NumberFormat.getInstance(fmtLocale);
+
+        onView(withId(R.id.indicator_txt)).check(matches(withText(formatter.format(-1.5))));
+    }
+
+    @Test
+    public void addSubTest() {
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.btn_8), withText(R.string.eight),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                8),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.btn_push), withText(R.string.push),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                17),
+                        isDisplayed()));
+        appCompatButton2.perform(click());
+
+        ViewInteraction appCompatButton3 = onView(
+                allOf(withId(R.id.btn_6), withText(R.string.six),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                6),
+                        isDisplayed()));
+        appCompatButton3.perform(click());
+
+        ViewInteraction appCompatButton4 = onView(
+                allOf(withId(R.id.btn_push), withText(R.string.push),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                17),
+                        isDisplayed()));
+        appCompatButton4.perform(click());
+
+        ViewInteraction appCompatButton5 = onView(
+                allOf(withId(R.id.btn_add), withText(R.string.plus),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                12),
+                        isDisplayed()));
+        appCompatButton5.perform(click());
+
+        ViewInteraction appCompatButton6 = onView(
+                allOf(withId(R.id.btn_sub), withText(R.string.minus),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                13),
+                        isDisplayed()));
+        appCompatButton6.perform(click());
+        onView(withId(R.id.indicator_txt)).check(matches(withText("-4")));
+    }
+
+    @Test
+    public void mulTest() {
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.btn_5), withText(R.string.five),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                5),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.btn_push), withText(R.string.push),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                17),
+                        isDisplayed()));
+        appCompatButton2.perform(click());
+
+        ViewInteraction appCompatButton3 = onView(
+                allOf(withId(R.id.btn_4), withText(R.string.four),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                4),
+                        isDisplayed()));
+        appCompatButton3.perform(click());
+
+        ViewInteraction appCompatButton4 = onView(
+                allOf(withId(R.id.btn_neg), withText(R.string.neg),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                16),
+                        isDisplayed()));
+        appCompatButton4.perform(click());
+
+        ViewInteraction appCompatButton5 = onView(
+                allOf(withId(R.id.btn_mul), withText(R.string.mul),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                14),
+                        isDisplayed()));
+        appCompatButton5.perform(click());
+        onView(withId(R.id.indicator_txt)).check(matches(withText("-20")));
+    }
+
+    @Test
+    public void divByZeroTest() {
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.btn_1), withText(R.string.one),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                1),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.btn_push), withText(R.string.push),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                17),
+                        isDisplayed()));
+        appCompatButton2.perform(click());
+
+        ViewInteraction appCompatButton3 = onView(
+                allOf(withId(R.id.btn_0), withText(R.string.zero),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                10),
+                        isDisplayed()));
+        appCompatButton3.perform(click());
+
+        ViewInteraction appCompatButton4 = onView(
+                allOf(withId(R.id.btn_div), withText(R.string.div),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                15),
+                        isDisplayed()));
+        appCompatButton4.perform(click());
+        onView(withId(R.id.indicator_txt)).check(matches(withText("∞")));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
